44306a499a739125b83d043204cb06ce75b280c3
5aim <hyukjin720@gmail.com>
2022-05-10 15:45:11 +0900

    개별프로필 삭제 / 파이썬 병합(연결되는 script는 미완) / 메인페이지 script 추가 /  {{ 필요부분 : 함수이름 해당 div에 추가해야함 & db구성 참고한 API값 추가 }}

--- app.py
+++ app.py
@@ -1,68 +1,234 @@
-from flask import Flask, render_template, request, jsonify
-
-app = Flask(__name__)
-
 from pymongo import MongoClient
+import jwt
+import datetime
+import hashlib
 import certifi
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
 
 ca = certifi.where()
 
-client = MongoClient('mongodb+srv://text:sparta@cluster0.ezoan.mongodb.net/Cluster0?retryWrites=true&w=majority',
-                     tlsCAFile=ca)
+client = MongoClient('mongodb+srv://text:sparta@cluster0.ezoan.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.mdx5k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 db = client.dbsparta
 
-# 메인페이지 html 불러오기
+
+@app.route('/')
+def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        # token을 시크릿키로 복호화
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('mainpage.html', user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('loginpage.html', msg=msg)
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup_page.html')
+
+
+@app.route('/user/<username>')
+def user(username):
+    # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
+
+        user_info = db.users.find_one({"username": username}, {"_id": False})
+        return render_template('profilepage.html', user_info=user_info, status=status)
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    # 회원가입 때와 같은 방법으로 pw를 암호화한다.
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    # id, 암호화된 pw를 가지고 해당 유저를 찾는다.
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+            # JWT 토큰에는, payload와 시크릿키가 필요하다.
+            # 시크릿키가 있어야 토큰을 복호화해서 payload 값을 볼 수 있다.
+            # id와 exp를 담는다. JWT 토큰을 풀면 유저ID 값을 알 수 있다.
+            # exp에는 만료시간을 넣어준다. 만료시간이 지나면, 시크릿키로 토큰을 풀때 만료되었다고 에러가 난다.
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        # 토큰을 줍니다.
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    usermail_receive = request.form['usermail_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,     # 아이디
+        "usermail": usermail_receive,     # 이메일
+        "password": password_hash,        # 비밀번호
+        "profile_name": username_receive, # 프로필 이름 기본갑
+        "profile_pic": "",                # 프로필 사진 파일
+        "profile_pic_real": "profile_pics.profile_placeholder.png",  # 프로필 사진 기본
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+@app.route('/posting', methods=['POST'])
+def posting():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        # 포스팅(댓글)하기
+        user_info = db.users.find_one({"username": payload["id"]})
+        comment_receive = request.form["comment_give"]
+        date_receive = request.form["date_give"]
+        print(type(date_receive))
+        doc = {
+            "username": user_info["username"],
+            "profile_name": user_info["profile_name"],
+            "profile_pic_real": user_info["profile_pic_real"],
+            "comment": comment_receive,
+            "date": date_receive
+        }
+        db.posts.insert_one(doc)
+        return jsonify({"result": "success", 'msg': '댓글 성공'})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+@app.route("/get_posts", methods=['GET'])
+def get_posts():
+    # 댓글 받아오는 get
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        my_username = payload["id"]
+        username_receive = request.args.get("username_give")
+        if username_receive=="":
+            posts = list(db.posts.find({}).sort("date", -1).limit(20))
+        else:
+            posts = list(db.posts.find({"username":username_receive}).sort("date", -1).limit(20))
+
+        for post in posts:
+            post["_id"] = str(post["_id"])
+
+        return jsonify({"result": "success", "msg": "댓글을 가져왔습니다.", "posts": posts})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+
+""" 좋아요 수 계산 및 이미지 세이브 파일 아직 진행중
+@app.route('/update_like', methods=['POST'])
+def update_like():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        # 좋아요 수 변경
+        user_info = db.users.find_one({"username": payload["id"]})
+        post_id_receive = request.form["post_id_give"]
+        type_receive = request.form["type_give"]
+        action_receive = request.form["action_give"]
+        doc = {
+            "post_id": post_id_receive,
+            "username": user_info["username"],
+            "type": type_receive
+        }
+        if action_receive =="like":
+            db.likes.insert_one(doc)
+        else:
+            db.likes.delete_one(doc)
+        count = db.likes.count_documents({"post_id": post_id_receive, "type": type_receive})
+        print(count)
+        return jsonify({"result": "success", 'msg': 'updated', "count": count})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+@app.route('/update_profile', methods=['POST'])
+def save_img():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        username = payload["id"]
+        name_receive = request.form["name_give"]
+        about_receive = request.form["about_give"]
+        new_doc = {
+            "profile_name": name_receive,
+            "profile_info": about_receive
+        }
+        if 'file_give' in request.files:
+            file = request.files["file_give"]
+            filename = secure_filename(file.filename)
+            extension = filename.split(".")[-1]
+            file_path = f"profile_pics/{username}.{extension}"
+            file.save("./static/"+file_path)
+            new_doc["profile_pic"] = filename
+            new_doc["profile_pic_real"] = file_path
+        db.users.update_one({'username': payload['id']}, {'$set':new_doc})
+        return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+"""
+
+
+# 업로드 업로드 업로드 업로드 업로드 업로드 업로드 업로드 업로드 업로드
 
 @app.route('/')
 def home():
     return render_template('mainpage.html')
 
-# 불러온 메인페이지 html 페이지에 post 요청으로 댓글을 집어넣기
-# /instacommets 로 이름을 지어주고
-# dbprac 인써트 삽입하기를 불러와주고
-# doc 변수를 정해줌. 넘버를 달아서 댓글 순서를 찍어줘야 나중에 삭제할 때도 그거 하나만 삭제할 수 있음.
-# (코멘트 리스트를 리스트로 변수정의하고 그걸 len을 써서 +1을 더해 1, 2, 3 넘버를 만들어 카운트로 정의해줌)
-# comments를 commentes_receive로 받고
-# delete도 만들어줌. 일단 0으로 두고
-# 다음 html로 넘어가서
+@app.route('upload', methods=['POST'])
+def UploadReceive():
+    token_receive = request.cookies.get('mytoken')
+    payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+    user_info = db.users.find_one({"user_email": payload['id']})
 
-@app.route("/instacomments", methods=["POST"])
-def comments_post():
-    comments_receive = request.form['comments_give']
-
-    comments_list = list(db.comments.find({}, {'_id':False}))
-    count = len(comments_list) + 1
+    picture_receive = request.form['fd']
+    contents_receive = request.form['fd']
+    profile_receive = request.form['fd']
+    userID_receive = user_info['_id']
 
     doc = {
-        'num':count,
-        'comments':comments_receive,
-        'delete':0
+        'image': picture_receive,
+        'content': contents_receive,
+        'user_id': userID_receive,
+        'profile_image': profile_receive,
+        'feed_time': dt.datetime.utcnow()
     }
-
-    db.comments.insert_one(doc)
-
-    return jsonify({'msg': 'POST(기록) 연결 완료!'})
-
-
-@app.route("/instacomments", methods=["POST"])
-def comments_delete():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'POST(완료) 연결 완료!'})
-
-
-# get 요청은 쉬움. 코멘트 리스트로 리스틀 쭉 불러오고
-# html 작업으로 넘어가서
-
-@app.route("/isntacomments", methods=["GET"])
-def comments_get():
-    comments_list = list(db.comments.find({}, {'_id':False}))
-
-
-    return jsonify({'comments':comments_list})
-
+    db.feeds.insert_one(doc)
+    return jsonify({'result': 'success', 'msg': '게시물이 업로드 되었습니다.'})
 
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5200, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
